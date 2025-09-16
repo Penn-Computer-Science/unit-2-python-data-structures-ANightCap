0e874108831143f53d00959abbee451df6beafd6
Kaden <kbalentine376@phm.k12.in.us>
2025-09-16 09:48:53 -0400

    Update gradebookManager.py

--- gradebookManager.py
+++ gradebookManager.py
@@ -1,2 +1,18 @@
+while True:
+    gradebook = input("Would you like to view grades or the students? Grades/Students: ").capitalize()
+    if gradebook == "Grades":
+        print("Here are the grades.")
+        break
+    elif gradebook == "Students":
+        print("Here are the students.")
+        break
+    else:
+        print("Please try again, I did not understand that.")
 
-#change
+if gradebook == "Grades":
+    print()
+    print("Test")
+
+if gradebook == "Students":
+    print()
+    print("Test")
