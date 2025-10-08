3b04f1e089bc0424d9ae01363b5e9cef157d4914
Kaden <kbalentine376@phm.k12.in.us>
2025-10-08 09:19:35 -0400

    Update MPL.py

--- MPL.py
+++ MPL.py
@@ -1,32 +1,10 @@
 import pandas as pd
-import random
 import matplotlib.pyplot as plt
 
-df = pd.DataFrame()
-
-fNames = ["Mac", "Danielle", "Aiden", "Ben", "Van", "Kaden", "Lucian", "Andrew", "Benedict", "Keaton", "Joe", "Noah", "Roman", "Kyan", "Elijah", "Alexis", "Katerina", "Jill", "Jane", "Emmy", "Emma", "Eliana", "Rachel", "Marge", "Lisa" ]
-lNames = ["Smith", "Jones", "Johnson", "Willaims", "Brown", "Garcia", "Davis",  "Miller", "Wilson", "Moore", "Foster", "Anderson", "Manukyan", "DeFreitas", "Fogarty"]
-years = ["Freshman", "Sophomore", "Junior", "Senior", "Victory Lap"]
-pathways = ["Early College", "Engineering", "Computer Science", "Business", "Marketing", "Early Childhood Education", "Culinary", "Criminal Justice", "Construction", "Bio Med"]
-names = []
-
-for i in range(2000):
-    names.append(f"{random.choice(fNames)} {random.choice(lNames)}")
-
-data = {
-    "Name": names,
-    "Age": [random.randint(14, 19) for _ in names],
-    "GPA": [round(random.uniform(0.3, 4.3),2) for _ in names],
-    "Credits Completed": [random.randint(0, 60) for _ in names],
-    "Year": [random.choice(years) for _ in names],
-    "Pathway": [random.choice(pathways) for _ in names]
-}
-
-pennData = pd.DataFrame(data)
-
-pennData.to_csv('students_dat00.csv', index = False) # Save to CSV file
+df = pd.DataFrame(pennData)
 
 print(df.describe())
 print(df.info())
-print(df.groupby('Year')['GPA'].mean())
-plt.show
+
+df.groupby('Year')['GPA'].mean()
+plt.show()
